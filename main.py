bde83ff00bcf789f803631bb2ed546be326e1b4f
Sevastyan Basharin <sv.acid6@gmail.com>
2019-02-15 11:20:07 +0400

    main

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import os
 pygame.init()
 size = 720, 480
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    clock.tick(fps)
+    pygame.display.flip()
 
 
 
